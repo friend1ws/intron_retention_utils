3fca27fcd3b150b4fefa432e1c19ee628bc5604b
friend1ws <friend1ws@gmail.com>
2015-11-17 12:55:32 +0900

    make setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='genomon_intron_retention',
+      version='0.1.0',
+      description='Python tools for extracting intron retention events',
+      author='Yuichi Shiraishi',
+      author_email='friend1ws@gamil.com',
+      url='https://github.com/Genomon-Project/GenomonIntronRetention.git',
+      package_dir = {'': 'lib'},
+      packages=['genomon_intron_retention'],
+      scripts=['genomon_intron_retention'],
+      license='GPL-3'
+     )
+
