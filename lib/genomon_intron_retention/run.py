b0e8923ccc8affd43dc953077a3d7a652b9fd585
friend1ws <friend1ws@gmail.com>
2016-08-09 23:06:33 +0900

    remove bedtools path from the arguments

--- lib/genomon_intron_retention/run.py
+++ lib/genomon_intron_retention/run.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 
 import sys, os, subprocess
-import utils, intron_db, mutation, allele_count, pyssw
+import utils, intron_db
 
 def simple_count_main(args):
 
     input_bam = args.bam_file
     output_prefix = args.output_prefix
     annotation_dir = args.annotation_dir
-    bedtools_path = args.bedtools_path
+    # bedtools_path = args.bedtools_path
     mapq_thres = args.q
 
     output_prefix_dir = os.path.dirname(output_prefix)
@@ -19,28 +19,29 @@ def simple_count_main(args):
     utils.filterImproper(input_bam, output_prefix + ".filt.bam", mapq_thres)
 
 
-    hOUT = open(output_prefix + ".filt.bed12", 'w')
-    s_ret = subprocess.call([bedtools_path + "/bedtools", "bamtobed", "-bed12", "-i", output_prefix + ".filt.bam"], stdout = hOUT)
-    hOUT.close()
+    hout = open(output_prefix + ".filt.bed12", 'w')
+    s_ret = subprocess.call(["bedtools", "bamtobed", "-bed12", "-i", output_prefix + ".filt.bam"], stdout = hout)
+    hout.close()
 
     if s_ret != 0:
         print >> sys.stderr, "error in generating filt.bed12"
         sys.exit(1)
 
 
-    hOUT = open(output_prefix + ".edge.bed", 'w')
-    s_ret = subprocess.call([bedtools_path + "/bedtools", "intersect", "-a", output_prefix + ".filt.bed12", "-b", annotation_dir + "/edge.bed", 
-                     "-split", "-wao"], stdout = hOUT)
-    hOUT.close()
+    hout = open(output_prefix + ".edge.bed", 'w')
+    s_ret = subprocess.call(["bedtools", "intersect", "-a", output_prefix + ".filt.bed12", "-b", annotation_dir + "/edge.bed", 
+                     "-split", "-wao"], stdout = hout)
+    hout.close()
 
     if s_ret != 0:
         print >> sys.stderr, "error in generating edge.bed"
         sys.exit(1)
 
-    hOUT = open(output_prefix + ".edge_broaden.bed", 'w')
-    s_ret = subprocess.call([bedtools_path + "/bedtools", "intersect", "-a", output_prefix + ".filt.bed12", "-b", annotation_dir + "/edge_broaden.bed", 
-                     "-split", "-wao"], stdout = hOUT)
-    hOUT.close()
+
+    hout = open(output_prefix + ".edge_broaden.bed", 'w')
+    s_ret = subprocess.call(["bedtools", "intersect", "-a", output_prefix + ".filt.bed12", "-b", annotation_dir + "/edge_broaden.bed", 
+                     "-split", "-wao"], stdout = hout)
+    hout.close()
 
     if s_ret != 0:
         print >> sys.stderr, "error in generating edge_broaden.bed"
@@ -57,6 +58,8 @@ def simple_count_main(args):
 
 
 def allele_count_main(args):
+
+    import mutation, allele_count, pyssw
 
     output_dir = os.path.dirname(args.output_file)
     if output_dir != "" and not os.path.exists(output_dir):
