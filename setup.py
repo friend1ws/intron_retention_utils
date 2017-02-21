58072ee966d41d8041c0c4f897bd41d146627323
friend1ws <friend1ws@gmail.com>
2017-02-21 17:06:30 +0900

    modify release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='intron_retention_utils',
-      version='0.3.0rc2',
+      version='0.3.0',
       description='Python tools for extracting intron retention events',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
