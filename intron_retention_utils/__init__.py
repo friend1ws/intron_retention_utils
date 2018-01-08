311854b362e1c51ccbbe6120d30a2fab5aac6b3a
Yuichi Shiraishi <friend1ws@gmail.com>
2018-01-08 10:40:07 +0900

    modify package structure

---
+++ intron_retention_utils/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from parser import create_parser
+
+def main():
+
+    parser = create_parser()
+    args = parser.parse_args()
+    args.func(args)
+
--- setup.py
+++ setup.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='intron_retention_utils',
-      version='0.3.1',
-      description='Python tools for extracting intron retention events',
-      author='Yuichi Shiraishi',
-      author_email='friend1ws@gamil.com',
-      url='https://github.com/friend1ws/intron_retention_utils',
-      package_dir = {'': 'lib'},
-      packages=['intron_retention_utils'],
-      scripts=['intron_retention_utils'],
-      license='GPL-3'
-     )
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'intron_retention_utils',
+    version = '0.4.0a1',
+    description='Python tools for extracting intron retention events',
+    url = 'https://github.com/friend1ws/intron_retention_utils',
+    author = 'Yuichi Shiraishi',
+    author_email = 'friend1ws@gamil.com',
+    license = 'GPLv3',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+
+    packages = find_packages(exclude = ['resource', 'fusion_db', 'cancer_gene']),
+    install_requires = [],
+    entry_points = {'console_scripts': ['intron_retention_utils = intron_retention_utils:main']}
+
+)
 
