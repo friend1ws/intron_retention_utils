2f78efc6ce9b9be7c07c3d294e63b98b345331c4
friend1ws <friend1ws@gmail.com>
2018-01-12 11:51:56 +0900

    add merge_control tests

---
+++ tests/test_merge_control.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import unittest
+import os, glob, tempfile, shutil, filecmp
+import intron_retention_utils
+from check_download import *
+
+class TestFilter(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = intron_retention_utils.parser.create_parser()
+
+
+    def tearDown(self):
+        pass 
+
+
+    def test1(self):
+
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = tempfile.mkdtemp()
+
+        all_simple_count_file = glob.glob(cur_dir + "/data/simple_count/*.ir_simple_count.result.txt")
+        with open(tmp_dir + "/CCLE.ir_simple_count.reulst_list.txt", 'w') as hout:
+            for simple_count_file in sorted(all_simple_count_file):
+                print >> hout, simple_count_file
+
+        input_list_file = tmp_dir + "/CCLE.ir_simple_count.reulst_list.txt"
+        output_file = tmp_dir + "/merge_control.bed.gz"
+        answer_file = cur_dir + "/data/merge_control/merge_control.bed.gz"
+ 
+        args = self.parser.parse_args(["merge_control", input_list_file, output_file])
+        args.func(args)
+
+        self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
+
+        shutil.rmtree(tmp_dir)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
