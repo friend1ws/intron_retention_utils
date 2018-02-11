b52c8de1de4ec24a471341106b9373181c17e5ab
friend1ws <friend1ws@gmail.com>
2018-02-11 17:42:06 +0900

    add sv associate files

--- tests/test_associate.py
+++ tests/test_associate.py
@@ -38,6 +38,25 @@ class TestAssociate(unittest.TestCase):
 
         shutil.rmtree(tmp_dir)
 
+
+    def test2(self):
+
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = tempfile.mkdtemp()
+
+        simple_count_file = cur_dir + "/data/simple_count/CCLE-GSU-RNA-08.ir_simple_count.txt"
+        sv_file = cur_dir + "/data/sv/CCLE-GSU-DNA-08.genomonSV.result.filt.txt"
+        output_file = tmp_dir + "/CCLE-GSU-RNA-08.ir_simple_count.associate.txt"
+        answer_file = cur_dir + "/data/associate/CCLE-GSU-RNA-08.ir_simple_count.associate.txt"
+
+        args = self.parser.parse_args(["associate", "--sv", simple_count_file, sv_file, output_file])
+        args.func(args)
+
+        self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
+
+        shutil.rmtree(tmp_dir)
+
+
 if __name__ == "__main__":
     unittest.main()
 
